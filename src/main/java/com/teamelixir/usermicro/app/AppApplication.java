ea159e8d5ae7bead5434521e0196fd87d4249775
Andy Sekulovski <andy.sek94@gmail.com>
2018-09-17 11:24:18 +1000

    gateway update

--- src/main/java/com/teamelixir/usermicro/app/AppApplication.java
+++ src/main/java/com/teamelixir/usermicro/app/AppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages={"com.teamelixir.usermicro.app"})
-@EnableDiscoveryClient
 @EnableJpaRepositories("com.teamelixir.usermicro.app.repository")
+@EnableDiscoveryClient
 public class AppApplication {
 
     public static void main(String[] args) {
